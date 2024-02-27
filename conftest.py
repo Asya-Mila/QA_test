7e4b2ad1e42df6a77c41b73708c7049599fabb7c
Anastasia Malgina <malgina.an@gmail.com>
2024-02-27 22:54:37 +0300

    add new fiels

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+@pytest.fixture # фикстура, которая создаёт книгу
+def book():
+    book = ['Алиса в стране чудес', 'Скотный двор', '1984']
+    return book
+
+
+@pytest.fixture
+def books_genre():
+    books_genre = ['Фантастика', 'Роман', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+    return books_genre
+
+
+@pytest.fixture
+def favorites_books():
+    favorites_books = ['Меч и магия', 'Джейн Эйр']
+    return favorites_books
